d01d5735db1280fa74b412fb5ecaf3b46bbe27d8
dastudent <l3farrow@gmail.com>
2020-04-08 09:57:43 +0100

    2nd draft testing score

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // function to calculate the result of the survey
-function tabulateAnswers() {
+// function tabulateAnswers() {
    
     var c1score = 0;
     var c2score = 0;
@@ -35,75 +35,237 @@ function tabulateAnswers() {
 //       if (choices[i].checked) {
 // ​
 //         if (choices[i].value == 'c1') {
-          if ('check1') {
-            return (maxscore += 1);
+function c1check1(){
+    return c1score =1
+}
+function c1check2(){
+    return c1score =2
+}
+function c1check3(){
+    return c1score =3
+}
+function c1check4(){
+    return c1score =4
+}
 
-          }else if ('check2'){
-            return (maxscore +=2);
+function c2check1(){
+    return c2score =1
+}
+function c2check2(){
+    return c2score =2
+}
+function c2check3(){
+    return c2score =3
+}
+function c2check4(){
+    return c2score =4
+}
 
-          }else if ('check3'){
-            return (maxscore +=3);
+function c3check1(){
+    return c3score =1
+}
+function c3check2(){
+    return c3score =2
+}
+function c3check3(){
+    return c3score =3
+}
+function c3check4(){
+    return c3score =4
+}
 
-          }else if ('check4'){
-              return (maxscore +=4);
-          }
+function c5check1(){
+    return c5score =1
+}
+function c5check2(){
+    return c5score =2
+}
+function c5check3(){
+    return c5score =3
+}
+function c5check4(){
+    return c5score =4
+}
+
+function c5check1(){
+    return c5score =1
+}
+function c5check2(){
+    return c5score =2
+}
+function c5check3(){
+    return c5score =3
+}
+function c5check4(){
+    return c5score =4
+}
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+// function c1check1(){
+//     return c1score =1
+// }
+// function c1check2(){
+//     return c1score =2
+// }
+// function c1check3(){
+//     return c1score =3
+// }
+// function c1check4(){
+//     return c1score =4
+// }
+
+        //     return (c1score +=1);
+
+        //   }else if ("c1Answer2"){
+        //     return (c1score+=2);
+
+        //   }else if ("c1Answer3"){
+        //     return (c1score +=3);
+
+        //   }else if ("c1Answer4"){
+        //       return (c1score +=4);
+        //   }
 // checks which button is clicked then adds a value to c1score based on button clicked.
 
 // i think we can remove choices part, if we copy paste this if/else selection for each
 // question. May want to substitue maxscore for c1score for individual totals, which would
 // help with changing clicks as i wrote above
           
-        }
-        if (choices[i].value == 'c2') {
-          c2score = c2score + 1;
-        }
-        if (choices[i].value == 'c3') {
-          c3score = c3score + 1;
-        }
-        if (choices[i].value == 'c4') {
-          c4score = c4score + 1;
-        }
-        if (choice[i].value == 'c5') {
-            c5score = c5score + 1;
-        }
-        if ()
+    //     }
+    //     if (choices[i].value == 'c2') {
+    //       c2score = c2score + 1;
+    //     }
+    //     if (choices[i].value == 'c3') {
+    //       c3score = c3score + 1;
+    //     }
+    //     if (choices[i].value == 'c4') {
+    //       c4score = c4score + 1;
+    //     }
+    //     if (choice[i].value == 'c5') {
+    //         c5score = c5score + 1;
+    //     }
+    //     if ()
         
-      }
-    }
+    //   }
+    // }
     
     
     
-    var maxscore = Math.max(c1score,c2score,c3score,c4score);
-    // may want to change it to maxscore = c1score+c2score etc -> c20score
-    // Display answer corresponding to that choice
-    var answerbox = document.getElementById('answer');
-    if (c1score == maxscore) { // If user chooses the first choice the most, this outcome will be displayed.
-      answerbox.innerHTML = 
-    }
-    if (c2score == maxscore) { // If user chooses the sec"You are an altruistic coder! Yond choice the most, this outcome will be displayed.
-      answerbox.innerHTML = 
-    }
-    if (c3score == maxscore) { // If user chooses the third choice the most, this outcome will be displayed.
-      answerbox.innerHTML = 
-    }
-    if (c4score == maxscore) { // If user chooses the fourth choice the most, this outcome will be displayed.
-      answerbox.innerHTML = 
-    }
-    // If you add more choices, you must add another response below.
-  }
+//     var maxscore = Math.max(c1score,c2score,c3score,c4score);
+//     // may want to change it to maxscore = c1score+c2score etc -> c20score
+//     // Display answer corresponding to that choice
+//     var answerbox = document.getElementById('answer');
+//     if (c1score == maxscore) { // If user chooses the first choice the most, this outcome will be displayed.
+//       answerbox.innerHTML = 
+//     }
+//     if (c2score == maxscore) { // If user chooses the sec"You are an altruistic coder! Yond choice the most, this outcome will be displayed.
+//       answerbox.innerHTML = 
+//     }
+//     if (c3score == maxscore) { // If user chooses the third choice the most, this outcome will be displayed.
+//       answerbox.innerHTML = 
+//     }
+//     if (c4score == maxscore) { // If user chooses the fourth choice the most, this outcome will be displayed.
+//       answerbox.innerHTML = 
+//     }
+//     // If you add more choices, you must add another response below.
+//   }
 //   then display this here instead:
 
+var maxscore=(c1score+c2score+c3score+c4score+c5score)
+
 if (maxscore >=10 && maxscore <18)
-  answerbox.innerHTML="Your social style is Analytical!"
+document.getElementById("score").innerHTML="Your social style is Analytical!"
   if (maxscore >=18 && maxscore <25)
-  answerbox.innerHTML="Your social style is Amiable!"
+  document.getElementById("score").innerHTML="Your social style is Amiable!"
   if (maxscore >=25 && maxscore <32)
-  answerbox.innerHTML="Your social style is Expressive!"
+  document.getElementById("score").innerHTML="Your social style is Expressive!"
   if (maxscore >=32 && maxscore <=40)
-  answerbox.innerHTML="Your social style is Driver!" 
+  document.getElementById("score").innerHTML="Your social style is Driver!" 
 
   // program the reset button
-  function resetAnswer() {
-    var answerbox = document.getElementById('answer');
-    answerbox.innerHTML = "Your result will show up here!";
-  }
+//   function resetAnswer() {
+//     var answerbox = document.getElementById('answer');
+//     answerbox.innerHTML = "Your result will show up here!";
+//   }
